d8df5a6267edeb0ae66a3a45fc98cf72e5566260
Tarlan Soltanov <tarlansoltanov@outlook.com>
2022-06-04 20:39:52 +0200

    51. N-Queens - Python

---
+++ 51. N-Queens/Solution.py
@@ -0,0 +1,22 @@
+# Author : tarlansoltanov
+# Name : N-Queens
+# ID : 51
+# Link : https://leetcode.com/problems/n-queens/
+# Level : Hard
+# Language : Python
+
+from itertools import permutations
+    
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        result = []
+        rows = list(range(n))
+        for cols in permutations(range(n)):
+            first = set()
+            second = set() 
+            for (i, j) in zip(rows, cols):
+                first.add(i - j)
+                second.add(i + j)
+            if len(first) == n and len(second) == n:
+                result.append([j * '.' + 'Q' + (n - j - 1) * '.' for j in cols])
+        return result
