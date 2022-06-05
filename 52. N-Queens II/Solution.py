bdd45de64075e1b17eb7455497dcb90d44a31977
Tarlan Soltanov <tarlansoltanov@outlook.com>
2022-06-05 20:41:52 +0200

    52. N-Queens II - Python

---
+++ 52. N-Queens II/Solution.py
@@ -0,0 +1,21 @@
+# Author : tarlansoltanov
+# Name : N-Queens II
+# ID : 52
+# Link : https://leetcode.com/problems/n-queens-ii/
+# Level : Hard
+# Language : Python
+
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        count = 0
+        rows = list(range(n))
+        for cols in permutations(range(n)):
+            first = set()
+            second = set()
+            for (i, j) in zip(rows, cols):
+                first.add(i - j)
+                second.add(i + j)
+            if len(first) == n and len(second) == n: 
+                count += 1
+        
+        return count
