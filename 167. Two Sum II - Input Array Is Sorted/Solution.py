a1096f697918628c7487846452952df1428e604b
Tarlan Soltanov <tarlansoltanov@outlook.com>
2022-06-09 22:44:48 +0200

    167. Two Sum II - Input Array Is Sorted - Python - Update

--- 167. Two Sum II - Input Array Is Sorted/Solution.py
+++ 167. Two Sum II - Input Array Is Sorted/Solution.py
@@ -5,27 +5,17 @@
 # Level : Medium
 # Language : Python
 
-def indexOf(numbers: List[int], target: int) -> int:
-    n = len(numbers)
-    if n < 2:
-        return - 1 if not n or (numbers[0] != target) else 0
-
-    middle = (len(numbers)-1) // 2
-
-    if numbers[middle] > target:
-        index = indexOf(numbers[:middle], target)
-        return index if index != -1 else -1
-
-    if numbers[middle] < target:
-        index = indexOf(numbers[middle+1::], target)
-        return index + middle + 1 if index != -1 else -1
-
-    return middle
-    
-
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        for i in range(len(numbers)):
-            index = indexOf(numbers[i+1::], target - numbers[i])
-            if index != -1:
-                return [i+1, index+i+2]
+        i, j = 0, len(numbers)-1
+
+        while i < j:
+            rem = target - (numbers[i] + numbers[j])
+            
+            if rem == 0:
+                return [i+1, j+1]
+            
+            print(i, j)
+            
+            i += (rem > 0)
+            j -= (rem < 0)
