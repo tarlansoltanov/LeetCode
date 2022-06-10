a5fd439708eca447a3785aa7dc0866806fd29ce0
Tarlan Soltanov <tarlansoltanov@outlook.com>
2022-06-10 22:47:04 +0200

    704. Binary Search - Python

---
+++ 704. Binary Search/Solution.py
@@ -0,0 +1,24 @@
+# Author : tarlansoltanov
+# Name : Binary Search
+# ID : 704
+# Link : https://leetcode.com/problems/binary-search/
+# Level : Easy
+# Language : Python
+
+class Solution:
+    def search(self, numbers: List[int], target: int) -> int:
+        n = len(numbers)
+        if n < 2:
+            return - 1 if not n or (numbers[0] != target) else 0
+
+        middle = (len(numbers)-1) // 2
+
+        if numbers[middle] > target:
+            index = self.search(numbers[:middle], target)
+            return index if index != -1 else -1
+
+        if numbers[middle] < target:
+            index = self.search(numbers[middle+1:], target)
+            return index + middle + 1 if index != -1 else -1
+
+        return middle
