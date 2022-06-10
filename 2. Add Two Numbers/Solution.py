2b8c9d9a6bef92b50d890be19c47a181d9b54362
Tarlan Soltanov <tarlansoltanov@outlook.com>
2022-06-10 18:47:59 +0200

    2. Add Two Numbers - Python

---
+++ 2. Add Two Numbers/Solution.py
@@ -0,0 +1,39 @@
+# Author : tarlansoltanov
+# Name : Add Two Numbers
+# ID : 2
+# Link : https://leetcode.com/problems/add-two-numbers/
+# Level : Medium
+# Language : Python
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        head, prev, cur = None, None, None
+        rem = 0
+        while l1 or l2:
+            cur = ListNode()
+            cur.val += l1.val if l1 else 0
+            cur.val += l2.val if l2 else 0
+            cur.val += rem
+            
+            rem = cur.val // 10
+            cur.val %= 10
+            
+            if prev:
+                prev.next = cur
+            else:
+                head = cur
+            prev = cur
+            
+            l1 = l1.next if l1 else l1
+            l2 = l2.next if l2 else l2
+            
+        if rem:
+            prev.next = ListNode(rem)
+            
+        return head
