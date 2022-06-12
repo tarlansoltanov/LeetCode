3a0a308876de549053c53db8c7de5bd8387d1ee7
Tarlan Soltanov <tarlansoltanov@outlook.com>
2022-06-12 15:38:50 +0200

    35. Search Insert Position - Python

---
+++ 35. Search Insert Position/Solution.py
@@ -0,0 +1,24 @@
+# Author : tarlansoltanov
+# Name : Search Insert Position
+# ID : 35
+# Link : https://leetcode.com/problems/search-insert-position/
+# Level : Easy
+# Language : Python
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        start, middle, end = 0, 0, len(nums) - 1
+        
+        while start <= end:
+            middle = (start + end) // 2
+            
+            if nums[middle] == target:
+                return middle
+            
+            if nums[middle] > target:
+                end = middle - 1
+            
+            else:
+                start = middle + 1
+                
+        return start     
