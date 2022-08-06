dae09f7a285cd8657110b6fe1bdb4205c337a655
Tarlan Soltanov <tarlansoltanov@outlook.com>
2022-08-07 00:06:41 +0400

    0852. Peak Index in a Mountain Array - Python

---
+++ 0852. Peak Index in a Mountain Array/Solution.py
@@ -0,0 +1,25 @@
+# Author        : tarlansoltanov
+# Title         : Peak Index in a Mountain Array
+# ID            : 852
+# URL           : https://leetcode.com/problems/peak-index-in-a-mountain-array/
+# Difficulty    : Medium
+# Language      : Python
+
+
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        start, middle, end, ans = 0, 0, len(arr) - 1, 0
+        
+        while start <= end:
+            middle = (start + end) // 2
+            
+            if arr[middle] > arr[ans]:
+                ans = middle
+            
+            if middle != 0 and arr[middle - 1] >= arr[middle]:
+                end = middle - 1
+            
+            else:
+                start = middle + 1
+                
+        return ans
