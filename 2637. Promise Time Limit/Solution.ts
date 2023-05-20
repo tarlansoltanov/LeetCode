ef31336209f6c89fc7f40b8913c8bbbc3a17b9b5
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-05-20 14:34:10 +0200

    2637. Promise Time Limit - TypeScript

---
+++ 2637. Promise Time Limit/Solution.ts
@@ -0,0 +1,27 @@
+// Author        : tarlansoltanov
+// Title         : Promise Time Limit
+// ID            : 2637
+// URL           : https://leetcode.com/problems/promise-time-limit/
+// Difficulty    : Easy
+// Language      : TypeScript
+
+
+type Fn = (...params: any[]) => Promise<any>;
+
+function timeLimit(fn: Fn, t: number): Fn {
+  return async function (...args) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject('Time Limit Exceeded');
+      }, t);
+      fn(...args)
+        .then(resolve)
+        .catch(reject);
+    });
+  };
+}
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
