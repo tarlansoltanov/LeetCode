8cba4db76e0cef1e1f2f6c75a49b16425cd7210d
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-05-27 14:37:34 +0200

    2704. To Be Or Not To Be - JavaScript

---
+++ 2704. To Be Or Not To Be/Solution.js
@@ -0,0 +1,24 @@
+// Author        : tarlansoltanov
+// Title         : To Be Or Not To Be
+// ID            : 2704
+// URL           : https://leetcode.com/problems/to-be-or-not-to-be/
+// Difficulty    : Easy
+// Language      : JavaScript
+
+
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function (val) {
+  const item = val;
+  return {
+    toBe: (val) => { if (item === val) { return true } else { throw new Error("Not Equal") } },
+    notToBe: (val) => { if (item !== val) { return true } else { throw new Error("Equal") } }
+  }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
