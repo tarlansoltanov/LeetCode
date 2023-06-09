a17f1417a9835bd6ae40b973d9833ea689786f9c
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-06-09 11:52:59 +0200

    2726. Calculator with Method Chaining - TypeScript

---
+++ 2726. Calculator with Method Chaining/Solution.ts
@@ -0,0 +1,44 @@
+// Author        : tarlansoltanov
+// Title         : Calculator with Method Chaining
+// ID            : 2726
+// URL           : https://leetcode.com/problems/calculator-with-method-chaining/
+// Difficulty    : Easy
+// Language      : TypeScript
+
+
+class Calculator {
+  result: number = 0;
+  constructor(value: number) {
+    this.result = value;
+  }
+
+  add(value: number): Calculator {
+    this.result += value;
+    return this;
+  }
+
+  subtract(value: number): Calculator {
+    this.result -= value;
+    return this;
+  }
+
+  multiply(value: number): Calculator {
+    this.result *= value;
+    return this;
+  }
+
+  divide(value: number): Calculator {
+    if (value == 0) throw Error('Division by zero is not allowed');
+    this.result /= value;
+    return this;
+  }
+
+  power(value: number): Calculator {
+    this.result **= value;
+    return this;
+  }
+
+  getResult(): number {
+    return this.result;
+  }
+}
